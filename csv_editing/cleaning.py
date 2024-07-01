8e049cf725e5476b7eb74b3329b2cae09f039730
Annarhysa Albert <annarhysa13@gmail.com>
2024-07-01 10:03:54 +0530

    Making neater directory

--- csv_editing/cleaning.py
+++ csv_editing/cleaning.py
@@ -1,12 +1,12 @@
 import pandas as pd
-df = pd.read_csv('data/all.csv')
+df = pd.read_csv('data/constituency.csv')
 
 #selecting on values that are necessary
 desired_columns = ['S.N.', 'Candidate', 'Party', 'EVM Votes', 'Postal Votes', 'Total Votes', '% of Votes']
 df_selected = df[desired_columns]
 
 # Save the new CSV
-df_selected.to_csv('data/all.csv', index=False)
+df_selected.to_csv('data/constituency.csv', index=False)
 
 #checking for null values
 null_values = df_selected.isnull().sum()
